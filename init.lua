4ab4d83c5ca4791f3efa1b8425ad792852ed48b5
richelle <sutherland.richelle3@gmail.com>
2025-04-03 08:52:54 +0300

    Added directory structure

--- init.lua
+++ init.lua
@@ -19,11 +19,9 @@ vim.opt.mouse = "n"
 -- Tabs & Indentation
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
-
-vim.opt.expandtab = true
-
-vim.opt.autoindent = true
 vim.bo.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
 
 -- Line Wrapping
 vim.opt.wrap = false
